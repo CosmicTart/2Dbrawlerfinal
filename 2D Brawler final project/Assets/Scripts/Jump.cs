9ab50f43194ae11fd7cd968a518d36e5b575f797
CosmicTart <CosmicTart@users.noreply.github.com>
2020-04-29 04:25:52 -0700

    Added project into repository
    Yes.

---
+++ 2D Brawler final project/Assets/Scripts/Jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [Range(1, 10)]
+    public float jumpVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
---
+++ 2D Brawler final project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    public bool enableCrouch = false;
+    float horizontalMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+        if (enableCrouch)
+        {
+            if (Input.GetButtonDown("Crouch"))
+            {
+                crouch = true;
+            }
+            else if (Input.GetButtonUp("Crouch"))
+            {
+                crouch = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //move character
+        Input.GetAxisRaw("Horizontal");
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
